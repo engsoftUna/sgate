7ea72184eb484c9290b22326b50e84e12491e89f
engsoftUna <matheusac85@gmail.com>
2013-12-17 10:43:09 -0200

    Update Script_dbsgate.sql

--- Script_dbsgate.sql
+++ Script_dbsgate.sql
@@ -1,10 +1,10 @@
 CREATE TABLE tipoproduto (
-idtipo int identity PRIMARY KEY,
+idtipo int identity primary key,
 tipo varchar(250)
 )
 
 CREATE TABLE produto (
-idproduto int PRIMARY KEY,
+idproduto int identity primary key,
 descricao varchar(250),
 dataexpiracao datetime,
 valorproduto decimal(20,2),
@@ -28,7 +28,7 @@ ALTER TABLE itenspacote ADD FOREIGN KEY(idproduto) REFERENCES produto (idproduto
 
 
 CREATE TABLE cliente (
-       idcliente int identity PRIMARY KEY,
+       idcliente int identity primary key,
        nome varchar(250),
        cpf varchar(250),
        logradouro varchar(250),
@@ -38,3 +38,4 @@ CREATE TABLE cliente (
        uf varchar(250),
        email varchar(250)
 )
+
